36c3b467b41edd67052d4922c2631bfddbd1467d
Lucas <168497946+Luvzke@users.noreply.github.com>
2024-10-04 00:18:28 -0300

    Curso Java
    Files de java feitos...

---
+++ CursoJava/Aula02/src/Caneta.java
@@ -0,0 +1,26 @@
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    void status() {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tampada?" + this.tampada);
+    }
+
+    void rabiscar() {
+        System.out.println(this.tampada == true ? "ERRO! Não posso rabiscar enquanto estiver tampada!" : "Estou rabiscando!");
+    }
+
+    void tampar() {
+        this.tampada = true;
+    }
+
+    void destampar() {
+        this.tampada = false;
+    }
+}
