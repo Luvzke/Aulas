f6cbf14c7adaf3f958d02a801e19f7ff95aa7aa0
Lucas <168497946+Luvzke@users.noreply.github.com>
2024-10-05 14:54:50 -0300

    Aulas: 03 e 04

---
+++ CursoJava/Aula04/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NIC" , "Amarelo", 0.4f);
+        c1.status();
+        Caneta c2 = new Caneta("KK" , "Laranja", 1.5f);
+        c2.status();
+    }
+}
